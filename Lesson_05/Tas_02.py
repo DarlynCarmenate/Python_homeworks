3e4d240152c509dbeb07cc1f6d5fd60e387dbefa
darlyncarmenate <darlyncarmenate@gmail.com>
2022-07-27 00:30:41 +0200

    Lesson 05

--- Lesson_05/Tas_02.py
+++ Lesson_05/Tas_02.py
@@ -2,9 +2,9 @@
 
 from random import randint as rd
 
-arr = [['1', '2', '3'], 
-       ['4', '5', '6'], 
-       ['7', '8', '9']]
+arr = [['*', '*', '*'], 
+       ['*', '*', '*'], 
+       ['*', '*', '*']]
 for i in arr:
     print(*i)
 
@@ -12,26 +12,25 @@ count = 0
 end = False
 while end == False:
     if count % 2 == 0:
+        part = 'X'
         print('Cross turn')
-        x = int(input('Input x: '))
-        y = int(input('Input y: '))
-        arr[x - 1][y - 1] = 'X'
-        for i in arr:
-            print(*i)
-        count += 1
     else:
+        part = 'O'
         print('Zero turn')
-        x = int(input('Input x: '))
-        y = int(input('Input x: '))
-        arr[x - 1][y - 1] = 'O'
-        for i in arr:
-            print(*i)
-        count += 1
+    x = int(input('Input x: '))
+    y = int(input('Input y: '))
+    if arr[x - 1][y - 1] == part:
+        print('Already busy! You skip..')
+    elif arr[x - 1][y - 1] == '*':
+        arr[x - 1][y - 1] = part
+    for i in arr:
+        print(*i)
+    count += 1
     if count >= 4:
-        if arr[0][0] == arr[0][1] == arr[0][2] or arr[1][0] == arr[1][1] == arr[1][2] or \
-        arr[2][0] == arr[2][1] == arr[2][2] or arr[0][0] == arr[1][0] == arr[2][0] or \
-        arr[0][1] == arr[1][1] == arr[2][1] or arr[0][2] == arr[1][2] == arr[2][2] or \
-        arr[0][0] == arr[1][1] == arr[2][2] or arr[0][2] == arr[1][1] == arr[2][0]:
+        if arr[0][0] == arr[0][1] == arr[0][2] != '*' or arr[1][0] == arr[1][1] == arr[1][2] != '*' or \
+        arr[2][0] == arr[2][1] == arr[2][2] != '*' or arr[0][0] == arr[1][0] == arr[2][0] != '*' or \
+        arr[0][1] == arr[1][1] == arr[2][1] != '*' or arr[0][2] == arr[1][2] == arr[2][2] != '*' or \
+        arr[0][0] == arr[1][1] == arr[2][2] != '*' or arr[0][2] == arr[1][1] == arr[2][0] != '*':
             if count % 2 == 1:
                 print('Cross won!')
             else:
@@ -40,8 +39,4 @@ while end == False:
         elif count == 9:
             print('Dead heat')
             end = True
-
-    
-
-    
 
